de477e4b897d5cfaafac42ccf563ba6dc738322d
Oyelakin Dotun Peter <oyelakindotun@gmail.com>
2019-12-22 15:53:08 +0100

    Update and rename views.py to application.py

--- application.py
+++ application.py
@@ -1,4 +1,20 @@
 """
+The flask application package.
+"""
+
+from flask import Flask
+from flask_session import Session
+from tempfile import mkdtemp
+
+app = Flask(__name__)
+
+app.config["DEBUG"] = True
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+"""
 Routes and views for the flask application.
 """
 
